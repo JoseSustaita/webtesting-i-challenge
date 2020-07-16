fd264783a4d930d214abe6f5ddeaf3ebc90c80f8
Jose Sustaita <josesustaita70@gmail.com>
2020-07-15 22:42:20 -0400

    Repair method test added

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,42 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
+
+const test_items = [
+  {
+    name: "Low Item",
+    durability: 10,
+    enhancement: 0,
+  },
+  {
+    name: "Mid Item",
+    durability: 50,
+    enhancement: 10,
+  },
+  {
+    name: "Mid-High Item",
+    durability: 75,
+    enhancement: 15,
+  },
+  {
+    name: "Mid-High Item 2",
+    durability: 80,
+    enhancement: 17,
+  },
+  {
+    name: "High Item",
+    durability: 100,
+    enhancement: 20,
+  },
+];
+
 // test away!
+
+//Repair tests
+describe("item enhancer unit tests", () => {
+  it("repairs", () => {
+    expect(enhancer.repair(test_items[0]).durability).toBe(100);
+    expect(enhancer.repair(test_items[1]).durability).toBe(100);
+    expect(enhancer.repair(test_items[2]).durability).toBe(100);
+    expect(enhancer.repair(test_items[3]).durability).toBe(100);
+    expect(enhancer.repair(test_items[4]).durability).toBe(100);
+  });
+});
